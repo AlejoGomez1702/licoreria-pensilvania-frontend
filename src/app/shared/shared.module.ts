944689e71f329658a9fa0c2fda1adb78e59c785d
LUIS ALEJANDRO GOMEZ CASTANO <LAGOMCAS@davivienda.loc>
2021-08-15 20:11:44 -0500

    Estructura de core y shared module correctamente configurada

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IndexComponent } from './index/index.component';
-import { StylesComponent } from './index/styles/styles.component';
-import { ScriptsComponent } from './index/scripts/scripts.component';
-
-
 
 @NgModule({
   declarations: [
     IndexComponent,
-    StylesComponent,
-    ScriptsComponent
+    // StylesComponent,
   ],
   imports: [
     CommonModule,
@@ -20,7 +15,10 @@ import { ScriptsComponent } from './index/scripts/scripts.component';
   ],
   exports: [
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    // Components
+    IndexComponent,
+    // StylesComponent,
   ]
 })
 export class SharedModule { }
