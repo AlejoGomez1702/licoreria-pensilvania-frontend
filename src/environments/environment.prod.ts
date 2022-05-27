d24521aceb1cccc0c05f6890392bf8f9b6b8380d
LUIS ALEJANDRO GOMEZ CASTANO <LAGOMCAS@davivienda.loc>
2022-05-26 20:20:17 -0500

    Nuevamente en heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://aalejogomez1702-licoreria-pensilvania-backend-mai-ixxf7knz7a-wl.a.run.app/api'
+  API_URL: 'https://store-licoreria-backend.herokuapp.com/api'
 };
